6a64771e7bde4b646b76bc5947fad4ac8c3017b0
Amith Krishnan <Amith.Krishnan@target.com>
2015-06-12 23:23:07 -0500

    simple starter classes

---
+++ src/main/groovy/com/automata/expt/crawl/Crawler.groovy
@@ -0,0 +1,12 @@
+package com.automata.expt.crawl
+
+/**
+*  Created by Amith Krishnan on 6/12/15.
+*/
+class Crawler {
+
+    String crawl(String url){
+        return new URL(url).text
+    }
+
+}
---
+++ src/main/groovy/com/automata/expt/queue/Receiver.groovy
@@ -0,0 +1,12 @@
+package com.automata.expt.queue
+
+/**
+ * Created by Amith Krishnan on 6/12/15.
+ */
+class Receiver {
+
+    String processMessage(String receivedMessage){
+        return receivedMessage
+    }
+
+}
